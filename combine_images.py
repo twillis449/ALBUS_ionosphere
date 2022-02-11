e672d301a5d1071358a755410986d37faca52c0a
twillis449 <tony.willis.research@gmail.com>
2022-02-11 09:41:32 -0800

    now gets poligon data directly by function call

--- combine_images.py
+++ combine_images.py
@@ -2,42 +2,51 @@
 
 # a script to subtract specified areas from a radio_cutout field
 from astropy.io import fits
-from check_array import check_array
+from check_array import check_array, update_dimensions
 from process_polygon_data import *
-from skimage.draw import  polygon
+from skimage.draw import polygon as skimage_polygon
+from conv_image import convolve_image
 
-import os.path
+import os
 import numpy as np
 import math
 import sys
+import shutil
 
 
-def combine_images(filename, json_file, offset_flux,use_dilation):
+def make_simulated_noise(fits_file,std_dev):
+    hdu_list = fits.open(fits_file)
+    hdu = hdu_list[0]
+    conv_factor = 1
+    use_fft = 'T'
+    use_fft = 'F'
+    shape = hdu.data.shape
+    mean = 0.0   # some constant
+    print('std dev is ', std_dev)
+    if std_dev > 0.0:
+      std_dev = std_dev * 6.0
+      noise_image = np.random.normal(mean, std_dev, shape)
+      outfile = 'simulated_radio_noise.fits'
+      if os.path.isfile(outfile):
+          os.remove(outfile)
+      hdu.data = noise_image 
+      hdu.writeto(outfile, overwrite=True)
+      convolve_image(outfile, conv_factor, use_fft)
+    else:
+      print('no noise specified so nothing returned')
+      return
+
+def combine_images(filename, json_polygons, original_noise=0.0):
 # get input pounts interior to polygons
-    if use_dilation =='T':
-      print('using dilated images')
-      use_dilated = True
-    else:
-      use_dilated = False
-    offset_flux = float(offset_flux) / 1000.00
-    print('combine_images: offset flux (Jy):', offset_flux)
-    print('combine_images: use_dilation:', use_dilation)
-    points =  json_file
-    print('os.path.isfile(points)', os.path.isfile(points))
+    try:
+      polygon_list, coords = process_json_file(json_polygons)
+      num_polygons = len(polygon_list)
+      print('**************number of polygons', num_polygons)
+    except:
+      num_polygons = 0
 
-    json_file_exists = False
-    # Download the polygon points
-    if os.path.isfile(points):
-        print ("File exists")
-        polygon_list, coords = process_json_file(points)
-        num_polygons = len(polygon_list)
-        print('num_polygons', num_polygons)
-        json_file_exists = True
-    else:
-      print('Json file not found - so no processing to be done.')
-      print('Copying diffuse image to final image and then exiting')
-    print('json file status',  json_file_exists )
-    if json_file_exists:
+    
+    if num_polygons > 0:
       diffuse_fits_file = filename + '_diffuse_structure_dilated.fits'
       compact_fits_file = filename + '_compact_structure_dilated.fits'
       print('original compact image data  in file ', compact_fits_file)
@@ -45,8 +54,8 @@ def combine_images(filename, json_file, offset_flux,use_dilation):
 
       hdu_list = fits.open(diffuse_fits_file)
       hdu = hdu_list[0]
+      incoming_dimensions = hdu.header['NAXIS']
       diffuse_data = check_array(hdu.data) 
-
       hdu_list = fits.open(compact_fits_file)
       hdu = hdu_list[0]
       compact_data = check_array(hdu.data)
@@ -56,37 +65,39 @@ def combine_images(filename, json_file, offset_flux,use_dilation):
       for i in range(len(polygon_list)):
          result = polygon_list[i]
          x, y = result.exterior.coords.xy
-# switch x, y as retrieved x, y are matplotlib display coordinates
-         temp = x
-         x = y
-         y = temp
-         rr, cc = polygon(x,y, compact_data.shape)
+         rr, cc = skimage_polygon(x,y, compact_data.shape)
          img[rr, cc] = 1
-      if offset_flux > 0.0:
-        diffuse_data = diffuse_data + (compact_data - offset_flux) * img 
-      else:
-        compact_data_masked = compact_data * img
-        diffuse_data = diffuse_data + compact_data_masked
+      compact_data_masked = compact_data * img 
+      diffuse_data = diffuse_data + compact_data_masked 
 
+
+      diffuse_data = update_dimensions(diffuse_data,incoming_dimensions)
       hdu.data = diffuse_data
-      fits_file_out = filename + '_final_processed_image.fits'
+      hdu.header['DATAMIN'] = hdu.data.min()
+      hdu.header['DATAMAX'] = hdu.data.max()
+
+      fits_file_out = filename + '_final_image.fits'
       print('sending output to ', fits_file_out)
       hdu.writeto(fits_file_out, overwrite=True)
     else:
-# just copy the diffuse file to final processed file
+# just link the diffuse file to final processed file
       diffuse_fits_file = filename + '_diffuse_structure_dilated.fits'
-      hdu_list = fits.open(diffuse_fits_file)
-      hdu = hdu_list[0]
       fits_file_out = filename + '_final_processed_image.fits'
-      hdu.writeto(fits_file_out, overwrite=True)
+      print('making a symbolic link')
+      if os.path.isfile(fits_file_out):
+        os.remove(fits_file_out)
+      os.symlink(diffuse_fits_file , fits_file_out)
+
+# following is not worth the effort
+#   make_simulated_noise(diffuse_fits_file, std_dev=original_noise)
+
 
 def main( argv ):
   print('combine_images: doing image addition')
   filename = argv[1]
-  offset_flux = argv[2] # value in mJy to be omitted from source subtraction
   json_file = argv[3]
-  use_dilated_image = argv[4] 
-  combine_images(filename, json_file, offset_flux, use_dilated_image)
+  noise = argv[3]
+  combine_images(filename, json_polygons, original_noise=0.0)
 
 if __name__ == '__main__':
     main(sys.argv)
