36c62d6f222f6facb34491db8a96c3769363fe8d
bennahugo <bennahugo@gmail.com>
2022-09-28 21:07:33 +0200

    make test output path set by env variable when customized

--- acceptance_tests/test_rinex_autodownload_convert_v2_sutherland_mk_vla.py
+++ acceptance_tests/test_rinex_autodownload_convert_v2_sutherland_mk_vla.py
@@ -59,11 +59,14 @@ def __run_rinex_predict(LONG, LAT, HEIGHT, OBJECT, DATA_DIR, MAX_DIST, START_TIM
     print("getGPSIONO Total run time: %7.2f minutes" % duration)
 
 def __checkmake_outputdir():
-    if os.path.exists('/albus_waterhole'):  
-        if not os.path.exists('/albus_waterhole/.output'):
-            os.mkdir('/albus_waterhole/.output')
+    testoutdir = os.environ.get("ALBUS_TESTCASE_OUTPUT", "/albus_waterhole")
+    if os.path.exists(testoutdir):
+        outputhiddendir = os.path.join(testoutdir, '.output')  
+        if not os.path.exists(outputhiddendir):
+            os.mkdir(outputhiddendir)
     else:
-        raise RuntimeError("'/albus_waterhole' must be a valid directory -- are you running inside Docker?")
+        raise RuntimeError(f"'{testoutdir}' must be a valid directory -- are you running inside Docker, check mount path?")
+    return outputhiddendir
 
 def test_sutherland():
     # Sutherland ZA GPS SUTM receiver coordinates
@@ -73,8 +76,7 @@ def test_sutherland():
     OBJECT="Sutherland_3C286_Aug_14_2022"
     START_TIME="2022/08/14 14:43:33.4"
     END_TIME="2022/08/14 15:55:20.9"
-    __checkmake_outputdir()
-    DATA_DIR = '/albus_waterhole/.output/Sutherland_3C286_test_Aug_14_2022'
+    DATA_DIR = os.path.join(__checkmake_outputdir(), 'Sutherland_3C286_test_Aug_14_2022')
 
     MAX_DIST = 350E3
     __run_rinex_predict(LONG, LAT, HEIGHT, OBJECT, DATA_DIR, MAX_DIST, START_TIME, END_TIME)
@@ -88,8 +90,7 @@ def test_meerkat():
     OBJECT="MeerKat_3C286_Aug_14_2022"
     START_TIME="2022/08/14 14:43:33.4"
     END_TIME="2022/08/14 15:55:20.9"
-    __checkmake_outputdir()
-    DATA_DIR = '/albus_waterhole/.output/MeerKat_3C286_test_Aug_14_2022'
+    DATA_DIR = os.path.join(__checkmake_outputdir(), 'MeerKat_3C286_test_Aug_14_2022')
 
     MAX_DIST = 350E3
     __run_rinex_predict(LONG, LAT, HEIGHT, OBJECT, DATA_DIR, MAX_DIST, START_TIME, END_TIME)
@@ -101,8 +102,7 @@ def test_meerkat():
 #     # == 107:37:05.91 W 
 #     HEIGHT = 2115 	
 #     OBJECT="VLA_3C286_Aug_14_2022"
-#     __checkmake_outputdir()
-#     DATA_DIR = '/albus_waterhole/.output/VLA_3C286_test_Aug_14_2022'
+#     DATA_DIR = os.path.join(__checkmake_outputdir(), 'VLA_3C286_test_Aug_14_2022')
 #     START_TIME="2022/08/14 11:43:33.4"
 #     END_TIME="2022/08/14 16:55:20.9"
     
